c415200722a860cddbf6d1c8230206dbc0dfb40f
Philip Parker <philipparker@Parker-iMac.local>
2020-10-15 18:03:04 +1100

    full models

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,7 @@
+# None Target Functions ####
+z <- function(x) (x-mean(x,na.rm=TRUE))/sd(x, na.rm = TRUE)
+
+#Taget functions ####
 #' Extract and Merge Age 4 data
 #'
 #' @return data
@@ -46,7 +50,8 @@ age_4_data <- function(){
     )
   #Merge Data
   age_4_b <- left_join(age_4_b, age_0_b)
-  age_4_data <- bind_rows(age_4_k, age_4_b)
+  age_4_data <- bind_rows(age_4_k, age_4_b) %>%
+    select(-indig1, -indig2)
   # Return data
   return(age_4_data)
 }
@@ -63,16 +68,14 @@ age_8_data <- function(){
            stratum = stratum, par_sdq = easdqtb, par_social = eapsoc, par_peer = eapeer,
            par_conduct = eacondb,teach_sdq = etsdqtb,teach_social = etpsoc, 
            teach_peer = etpeer, teach_conduct = etcondb
-           ) %>%
-    mutate(cohort = 'K')
+           ) 
   # Age 8 data birth cohort
   age_8_b <- readit::readit(here::here("data","lsacgrb8.sas7bdat")) %>%
     select(cid = hicid, grade = epc06a1,state = estate,weight = eweight,
            stratum = stratum, par_sdq = easdqtb, par_social = eapsoc, 
            par_peer = eapeer, par_conduct = eacondb,
            teach_sdq = etsdqtb,teach_social = etpsoc, teach_peer = etpeer, teach_conduct = etcondb
-           ) %>%
-    mutate(cohort = 'B')
+           )
   # Merge data and return
   age_8_data <- bind_rows(age_8_k, age_8_b)
   return(age_8_data)
@@ -86,13 +89,13 @@ age_8_data <- function(){
 #'
 child_achievement_data <- function(){
   child_achievement_data <- readit::readit(here::here("data","lsacnaplan.sas7bdat")) %>%
-    select(cid = hicid, numeracy = y3num, read = y3read, writing = y3write, spelling = y3spel,
-           grammar = y3gram,status = y3status
+    select(cid = hicid, ach_numeracy = y3num, ach_read = y3read, ach_writing = y3write, ach_spelling = y3spel,
+           ach_grammar = y3gram,status = y3status
            ) %>%
-    mutate(across(numeracy:grammar,
+    mutate(across(ach_numeracy:ach_grammar,
                   ~replace(., . < 0, NA)
                   )
-           )
+           ) 
   
   return(child_achievement_data)
 }
@@ -115,14 +118,82 @@ school_achievement_data <- function(data = age_4_data){
   # Extract school data for cohort K
   school_data_k <- tmp %>%
     filter(calendar_year == 2008 & HICID %in% ids[[2]]) %>%
-    select(cid = HICID, sid = School_ID, sector = School_Sector_Code, ses_sch = School_ICSEA) %>%
-    mutate(cohort = 'K')
-  # Extract school data for cohort K
-  sData2008B <- tmp %>%
+    select(cid = HICID, sid = School_ID, sector = School_Sector_Code, ses_sch = School_ICSEA) 
+  # Extract school data for cohort b
+  school_data_b <- tmp %>%
     filter(calendar_year == 2010 & HICID %in% ids[[1]]) %>%
-    select(cid = HICID, sid = School_ID, sector = School_Sector_Code, ses_sch = School_ICSEA)%>%
-    mutate(cohort = 'B')
+    select(cid = HICID, sid = School_ID, sector = School_Sector_Code, ses_sch = School_ICSEA)
   # Merge and return
-  school_achievement_data <- bind_rows(sData2008K, sData2008B)
+  school_achievement_data <- bind_rows(school_data_k, school_data_b)
   return(school_achievement_data)
+}
+
+
+data <- function(data_list = list()){
+  child_data = reduce(data_list,left_join, by = "cid") %>%
+    mutate(
+      ses_sch = z(ses_sch),
+      ach = psych::principal(.[, str_detect(names(.), "^ach_")])$score %>% as.vector
+    ) %>%
+    filter(status < 4,
+           grade == 19,
+           !is.na(sid)) %>%
+    select(-starts_with("ach_"),
+           -ends_with("sdq"),
+           -grade,
+           -status) %>%
+    relocate(starts_with("prior"),.before = "par_social")
+  return(child_data)
+}
+
+make_codebook <- function(data){
+  dataMaid::makeCodebook(data,
+                         file = here::here("data",
+                                     glue::glue("{Sys.Date()}_codebook.Rmd")),
+                         replace=TRUE)
+  return(here::here("data",glue::glue("{Sys.Date()}_codebook.pdf")))
+}
+
+
+data_imp <- function(data){
+  set.seed(1234)
+  bound <- tibble(var = which(str_detect(names(data),"prior|teach|par_.*")),
+                  left = 0, right = 10) %>%
+    as.matrix()
+  data_imp <- Amelia::amelia(as.data.frame(data), m = 5,
+                           idvars = c('cid','weight','sid','stratum','state','sector'),
+                           noms = c('geo', 'indig', 'gender', 'lang', 'cohort'),bounds = bound
+  )
+  return(data_imp)
+}
+
+models <- function(d = data_imp, outcome = c("peer","social","conduct"), source = c("par","teach"), m = 5,
+                   delta = 0.95, iterations = 4000){
+  # data preparation: add censoring#
+  data = list()
+  for (i in 1:m){
+    tmp = data.frame(d$imputations[[i]])
+    data[[i]] = tmp %>%
+      select(
+        outcome = glue::glue("{source}_{outcome}"),
+        prior = glue::glue("prior_{outcome}"),
+        ses_sch, ses, cohort,gender,geo,
+        ach,sector, sid
+      ) %>%
+      mutate(
+        censored = case_when(
+          outcome == 0 ~ "left",
+          outcome == 10 ~ "right",
+          TRUE ~ 'none'
+        )
+      )
+    # Run model
+    m <- brms::brm_multiple(outcome|cens(censored)~ses_sch + ses + prior + cohort + gender + geo + 
+                               ach + sector +  (1|sid), data = data, cores =4,
+                            control = list(adapt_delta = delta), iter = iterations)
+    return(m)
+  }
+  
+  
+  
 }
---
+++ _targets.R
@@ -0,0 +1,59 @@
+library(targets)
+source(here::here("R","functions.R"))
+options(tidyverse.quiet = TRUE)
+tar_option_set(packages = c("readit", "tidyverse", "dataMaid","here", "glue"))
+tar_pipeline(
+  tar_target(
+    age_4_data,
+    age_4_data()
+  ) ,
+  tar_target(
+    age_8_data,
+    age_8_data()
+  ),
+  tar_target(
+    child_achievement_data,
+    child_achievement_data()
+  ),
+  tar_target(
+    school_achievement_data,
+    school_achievement_data(data = age_4_data)
+  ),
+  tar_target(
+    data,
+    data(data_list = list(age_4_data, age_8_data, child_achievement_data, school_achievement_data))
+  ),
+  tar_target(
+    codebook,
+    make_codebook(data),
+    format = "file"
+  ),
+  tar_target(
+    data_imp,
+    data_imp(data)
+  ),
+  tar_target(
+    peer_par,
+    models(data_imp, outcome = "peer", source = "par")
+  ),
+  tar_target(
+    conduct_par,
+    models(data_imp, outcome = "conduct", source = "par")
+  ),
+  tar_target(
+    social_par,
+    models(data_imp, outcome = "social", source = "par")
+  ),
+  tar_target(
+    peer_teach,
+    models(data_imp, outcome = "peer", source = "teach")
+  ),
+  tar_target(
+    conduct_teach,
+    models(data_imp, outcome = "conduct", source = "teach")
+  ),
+  tar_target(
+    social_teach,
+    models(data_imp, outcome = "social", source = "teach")
+  )
+)
